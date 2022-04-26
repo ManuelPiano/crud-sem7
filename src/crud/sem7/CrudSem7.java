c55ffdcdecf5e78718f8371c116af8a2d6bad84c
ManuelPiano <manuel.alvarado21@itca.edu.sv>
2022-04-26 15:12:23 -0600

    REPO PARA TRABAJO EN GRUPO

--- src/crud/sem7/CrudSem7.java
+++ src/crud/sem7/CrudSem7.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package crud.sem7;
 
 /**
@@ -14,7 +11,9 @@ public class CrudSem7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+
+// TODO code application logic here
     }
     
 }
