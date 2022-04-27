c9ae167de797ce381d32e88d4476dc2267abf1a2
ManuelPiano <manuel.alvarado21@itca.edu.sv>
2022-04-26 18:22:57 -0600

    Menu principal
    creacion de la clase menu principal

--- src/crud/sem7/CrudSem7.java
+++ src/crud/sem7/CrudSem7.java
@@ -1,19 +1,9 @@
 
 package crud.sem7;
 
-/**
- *
- * @author manuel
- */
 public class CrudSem7 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
-
-// TODO code application logic here
     }
     
 }
